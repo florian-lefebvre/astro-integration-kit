7b5ee7c9eba24e0aa0058c344b8cc305268df898
Florian Lefebvre <contact@florian-lefebvre.dev>
2024-01-24 10:49:23 +0100

    feat: add addVitePlugin utility (#18)

--- package/src/index.ts
+++ package/src/index.ts
@@ -1,4 +1,5 @@
 export { addVirtualImport } from "./utils/add-virtual-import.js";
+export { addVitePlugin } from "./utils/add-vite-plugin.js";
 export { createResolver } from "./utils/create-resolver.js";
 export { defineIntegration } from "./utils/define-integration.js";
 export { watchIntegration } from "./utils/watch-integration.js";
--- package/src/vanilla.ts
+++ package/src/vanilla.ts
@@ -1,2 +1,3 @@
 export { vanillaAddVirtualImport as addVirtualImport } from "./utils/add-virtual-import.js";
+export { vanillaAddVitePlugin as addVitePlugin } from "./utils/add-vite-plugin.js";
 export { vanillaWatchIntegration as watchIntegration } from "./utils/watch-integration.js";
