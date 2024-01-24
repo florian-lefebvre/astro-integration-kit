97de1ae19c1b0476930f9aab2ca6d50dbcb995ee
Jacob Jenkins <jacob_jenkins@live.com>
2024-01-24 16:12:06 +0000

    feat: add hasIntegration utility (#22)
    Co-authored-by: Florian Lefebvre <contact@florian-lefebvre.dev>

--- package/src/index.ts
+++ package/src/index.ts
@@ -2,4 +2,5 @@ export { addVirtualImport } from "./utils/add-virtual-import.js";
 export { addVitePlugin } from "./utils/add-vite-plugin.js";
 export { createResolver } from "./utils/create-resolver.js";
 export { defineIntegration } from "./utils/define-integration.js";
+export { hasIntegration } from "./utils/has-integration.js";
 export { watchIntegration } from "./utils/watch-integration.js";
--- package/src/vanilla.ts
+++ package/src/vanilla.ts
@@ -1,3 +1,4 @@
 export { vanillaAddVirtualImport as addVirtualImport } from "./utils/add-virtual-import.js";
 export { vanillaAddVitePlugin as addVitePlugin } from "./utils/add-vite-plugin.js";
+export { vanillaHasIntegration as hasIntegration } from "./utils/has-integration.js";
 export { vanillaWatchIntegration as watchIntegration } from "./utils/watch-integration.js";
