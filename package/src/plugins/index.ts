5ac53cae152a43e9e1639cb93bad39a031a3d224
Florian Lefebvre <contact@florian-lefebvre.dev>
2024-02-04 17:41:51 +0100

    feat: add addVitePluginPlugin

--- package/src/plugins/index.ts
+++ package/src/plugins/index.ts
@@ -1,6 +1,11 @@
 import { addDtsPlugin } from "./add-dts.js";
 import { addVirtualImportPlugin } from "./add-virtual-import.js";
+import { addVitePluginPlugin } from "./add-vite-plugin.js";
 
-export const corePlugins = [addDtsPlugin, addVirtualImportPlugin] as const;
+export const corePlugins = [
+	addDtsPlugin,
+	addVirtualImportPlugin,
+	addVitePluginPlugin,
+] as const;
 
-export { addDtsPlugin, addVirtualImportPlugin };
+export { addDtsPlugin, addVirtualImportPlugin, addVitePluginPlugin };
