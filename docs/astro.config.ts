a59be59e43943d9cec715022407e8f885483c37d
Florian Lefebvre <contact@florian-lefebvre.dev>
2024-02-04 22:37:22 +0100

    feat: work on docs

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -1,6 +1,19 @@
 import starlight from "@astrojs/starlight";
 import { defineConfig } from "astro/config";
 
+const badge = (type: "new" | "updated" | "soon") => ({
+	variant: {
+		new: "success" as const,
+		updated: "caution" as const,
+		soon: "tip" as const,
+	}[type],
+	text: {
+		new: "New",
+		updated: "Updated",
+		soon: "Soon",
+	}[type],
+});
+
 // https://astro.build/config
 export default defineConfig({
 	site: "https://astro-integration-kit.netlify.app",
@@ -12,6 +25,8 @@ export default defineConfig({
 			},
 			social: {
 				github: "https://github.com/florian-lefebvre/astro-integration-kit",
+				discord:
+					"https://discord.com/channels/830184174198718474/1197638002764152843",
 			},
 			editLink: {
 				baseUrl:
@@ -28,10 +43,12 @@ export default defineConfig({
 						{
 							label: "Usage",
 							link: "/getting-started/usage/",
+							badge: badge("updated"),
 						},
 						{
 							label: "Why Astro Integration Kit",
 							link: "/getting-started/why",
+							badge: badge("new"),
 						},
 						{
 							label: "Showcase",
@@ -40,8 +57,70 @@ export default defineConfig({
 					],
 				},
 				{
+					label: "Core",
+					items: [
+						{
+							label: "createResolver",
+							link: "/core/create-resolver/",
+						},
+						{
+							label: "defineIntegration",
+							link: "/core/define-integration/",
+							badge: badge("updated"),
+						},
+						{
+							label: "defineOptions",
+							link: "/core/define-options/",
+							badge: badge("new"),
+						},
+						{
+							label: "definePlugin",
+							link: "/core/define-plugin/",
+							badge: badge("new"),
+						},
+					],
+				},
+				{
 					label: "Utilities",
-					autogenerate: { directory: "utilities" },
+					items: [
+						{
+							label: "addDts",
+							link: "/utilities/add-dts/",
+							badge: badge("new"),
+						},
+						{
+							label: "addVirtualImport",
+							link: "/utilities/add-virtual-import/",
+						},
+						{
+							label: "addVitePlugin",
+							link: "/utilities/add-vite-plugin/",
+						},
+						{
+							label: "hasIntegration",
+							link: "/utilities/has-integration/",
+							badge: badge("updated"),
+						},
+						{
+							label: "watchIntegration",
+							link: "/utilities/watch-integration/",
+						},
+					],
+				},
+				{
+					label: "Guides",
+					items: [
+						{
+							label: "Authoring an integration",
+							link: "/guides/authoring-an-integration",
+							badge: badge("soon"),
+						},
+						{
+							label: "Authoring a plugin",
+							link: "/guides/authoring-a-plugin",
+							badge: badge("soon"),
+						},
+					],
 				},
 			],
 			lastUpdated: true,
