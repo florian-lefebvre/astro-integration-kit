214de9e24860ec17a629bcbd109f6dfebabd36ff
Bryce Russell <brycetrussell@gmail.com>
2024-02-29 12:22:36 -0600

    Update docs sidebar

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -108,8 +108,14 @@ export default defineConfig({
 							link: "/utilities/add-dts/",
 						},
 						{
+							label: "addVirtualImport",
+							link: "/utilities/add-virtual-import/",
+							badge: badge("removed")
+						},
+						{
 							label: "addVirtualImports",
 							link: "/utilities/add-virtual-imports/",
+							badge: badge("new")
 						},
 						{
 							label: "addVitePlugin",
