74cf471aeafb02c56c1e410e691b4f3bfad633ef
Bryce Russell <brycetrussell@gmail.com>
2024-02-29 21:57:16 -0600

    Update package/src/plugins/add-virtual-imports.ts
    Co-authored-by: Luiz Ferraz <luiz@lferraz.com>

--- package/src/plugins/add-virtual-imports.ts
+++ package/src/plugins/add-virtual-imports.ts
@@ -1,20 +1,18 @@
 import { definePlugin } from "../core/define-plugin.js";
 import { addVirtualImports } from "../utilities/add-virtual-imports.js";
 
-const pluginIndexMap: Record<string, number> = {};
-
 export const addVirtualImportsPlugin = definePlugin({
 	name: "addVirtualImports",
 	hook: "astro:config:setup",
 	implementation:
-		({ updateConfig }, { name }) =>
-		(imports: Record<string, string>) => {
-			pluginIndexMap[name] ??= -1;
-			pluginIndexMap[name]++;
-			addVirtualImports({
-				name: `${name}-${pluginIndexMap[name]}`,
-				imports,
-				updateConfig,
-			});
+		({ updateConfig }, { name }) => {
+		  let counter = 1;
+		  (imports: Record<string, string>) => {
+			  addVirtualImports({
+				  name: `${name}-${counter++}`,
+				  imports,
+				  updateConfig,
+			  });
+		  },
 		},
 });
