4e73656857d5b41642201a3ee49d5764a22b0783
Bryce Russell <brycetrussell@gmail.com>
2024-03-02 23:45:56 -0600

    Override `updateConfig` function

--- package/src/plugins/has-vite-plugin.ts
+++ package/src/plugins/has-vite-plugin.ts
@@ -1,14 +1,42 @@
+import type { AstroConfig } from "astro";
 import { definePlugin } from "../core/define-plugin.js";
-import { hasVitePlugin } from "../utilities/has-vite-plugin.js";
+
+function getPlugins(
+	store: Set<string>,
+	plugins: AstroConfig["vite"]["plugins"] | undefined,
+) {
+	if (plugins) {
+		for (const plugin of plugins) {
+			if (!plugin) continue;
+
+			if (Array.isArray(plugin)) {
+				getPlugins(store, plugin);
+				continue;
+			}
+
+			if (plugin instanceof Promise) {
+				continue;
+			}
+
+			store.add(plugin.name);
+		}
+	}
+	return store;
+}
 
 export const hasVitePluginPlugin = definePlugin({
 	name: "hasVitePlugin",
 	hook: "astro:config:setup",
-	implementation:
-		({ config }) =>
-		(name: string) =>
-			hasVitePlugin({
-				name,
-        config
-			}),
+	implementation: (astroConfig) => {
+		const plugins = getPlugins(new Set(), astroConfig.config.vite?.plugins);
+
+		const { updateConfig } = astroConfig;
+
+		astroConfig.updateConfig = (config) => {
+			getPlugins(plugins, config.vite?.plugins);
+			return updateConfig(config);
+		};
+
+		return (name: string) => plugins.has(name);
+	},
 });
