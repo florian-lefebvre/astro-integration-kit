90da7dcdb3120510d1c0903db4825ba46d390f22
Bryce Russell <brycetrussell@gmail.com>
2024-03-03 22:41:35 -0600

    Refactor `hasVitePlugin` utility

--- package/src/utilities/has-vite-plugin.ts
+++ package/src/utilities/has-vite-plugin.ts
@@ -1,36 +1,56 @@
-import type { HookParameters } from 'astro';
+import type { AstroConfig, HookParameters } from 'astro';
+import type { PluginOption } from 'vite';
 
-export const hasVitePlugin = ({
-  name,
-  config
-}: {
-  name: string,
-  config: HookParameters<"astro:config:setup">["config"],
-}): boolean => {
-	const plugins = config.vite?.plugins
+function getPluginNames(
+	plugins: AstroConfig["vite"]["plugins"],
+) {
+	const names: string[] = []
 
 	if (plugins) {
 		for (const plugin of plugins) {
-			if (!plugin) continue
-		
+			if (!plugin) continue;
+
 			if (Array.isArray(plugin)) {
-				return hasVitePlugin({
-					name,
-					config: {
-						vite: {
-							plugins: plugin
-						}
-					} as typeof config
-				});
+				names.push(...getPluginNames(plugin));
+				continue;
 			}
-		
+
 			if (plugin instanceof Promise) {
-				continue
+				continue;
 			}
-		
-			return name === plugin.name;
+
+			names.push(plugin.name);
 		}
 	}
 
-	return false;
+	return names;
+}
+
+export const hasVitePlugin = ({
+  plugin,
+  config
+}: {
+  plugin: string | PluginOption,
+  config: HookParameters<"astro:config:setup">["config"],
+}): boolean => {
+	if (!plugin || plugin instanceof Promise) return false
+
+	const currentPlugins = new Set(getPluginNames(config?.vite?.plugins))
+
+	let plugins = new Set<string>()
+
+	if (typeof plugin === "string") {
+		plugins.add(plugin)
+	}
+
+	if (typeof plugin === "object") {
+		if (Array.isArray(plugin)) {
+			const names = new Set(getPluginNames(plugin as PluginOption[]))
+			names.forEach(plugins.add, plugins)
+		} else {
+			plugins.add(plugin.name)
+		}
+	}
+
+	return [...plugins].some(name => currentPlugins.has(name));
 }
