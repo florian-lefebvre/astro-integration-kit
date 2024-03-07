32cef831c25b26e2475eaba72bb1e25a321ca655
Florian Lefebvre <contact@florian-lefebvre.dev>
2024-03-07 15:04:21 +0100

    feat: allow passing an array to addVirtualImports

--- package/src/plugins/add-virtual-imports.ts
+++ package/src/plugins/add-virtual-imports.ts
@@ -4,12 +4,13 @@ import { addVirtualImports } from "../utilities/add-virtual-imports.js";
 export const addVirtualImportsPlugin = definePlugin({
 	name: "addVirtualImports",
 	hook: "astro:config:setup",
-	implementation: ({ updateConfig }, { name }) => {
+	implementation: ({ config, updateConfig }, { name }) => {
 		let counter = 1;
-		return (imports: Record<string, string>) => {
+		return (imports: Parameters<typeof addVirtualImports>[0]["imports"]) => {
 			addVirtualImports({
 				name: `${name}-${counter++}`,
 				imports,
+				config,
 				updateConfig,
 			});
 		};
