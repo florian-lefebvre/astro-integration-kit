f3406ada21c5026f9fa80f79ca33eaad9db0bb60
Florian Lefebvre <contact@florian-lefebvre.dev>
2024-03-23 15:59:02 +0100

    feat: update playground

--- package/src/core/index.ts
+++ package/src/core/index.ts
@@ -3,3 +3,4 @@ export { defineIntegration } from "./define-integration.js";
 export { definePlugin } from "./define-plugin.js";
 export { defineUtility } from "./define-utility.js";
 export * from "./types.js";
+export * from "../utilities/index.js";
--- package/src/plugins/index.ts
+++ package/src/plugins/index.ts
@@ -1,7 +1,1 @@
-import { hasVitePluginPlugin } from "./has-vite-plugin.js";
-
-export const corePlugins = [
-	hasVitePluginPlugin, // Has to be the first plugin to detect Vite plugins added from inside the integration
-] as const;
-
-export { hasVitePluginPlugin };
+export { hasVitePluginPlugin } from "./has-vite-plugin.js";
