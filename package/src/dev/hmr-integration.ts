8faabee6c83ba6718fb484e9eb40925f34bdd2e4
Florian Lefebvre <contact@florian-lefebvre.dev>
2024-04-07 11:43:15 +0200

    feat: hmrIntegration

--- package/src/dev/hmr-integration.ts
+++ package/src/dev/hmr-integration.ts
@@ -1,5 +1,26 @@
 import { z } from "astro/zod";
 import { defineIntegration } from "../core/define-integration.js";
+import { isAbsolute } from "pathe";
+import { createResolver } from "../core/create-resolver.js";
+import { AstroError } from "astro/errors";
+import { watchDirectory } from "../utilities/watch-directory.js";
+
+const getDirectory = async (directory: string) => {
+	if (isAbsolute(directory)) {
+		return directory;
+	}
+
+	const { default: callsites } = await import("callsites");
+	const calls = callsites();
+	if (!calls[0]) {
+		throw new AstroError("TODO:");
+	}
+	const filename = calls[0].getFileName();
+	if (!filename) {
+		throw new AstroError("TODO:");
+	}
+	return createResolver(filename).resolve(directory);
+};
 
 export const hmrIntegration = defineIntegration({
 	name: "aik/hmr",
@@ -7,6 +28,11 @@ export const hmrIntegration = defineIntegration({
 		directory: z.string(),
 	}),
 	setup({ options }) {
-		return {};
+		return {
+			"astro:config:setup": async (params) => {
+				const directory = await getDirectory(options.directory);
+				watchDirectory(params, directory);
+			},
+		};
 	},
 });
