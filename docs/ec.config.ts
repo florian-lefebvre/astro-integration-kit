844c6f63af25c08a0526da0c75b5fd886b1f23bb
Oliver Speir <115520730+OliverSpeir@users.noreply.github.com>
2025-07-28 14:03:45 -0600

    format

--- docs/ec.config.ts
+++ docs/ec.config.ts
@@ -2,12 +2,12 @@ import { pluginLineNumbers } from "@expressive-code/plugin-line-numbers";
 
 /** @type {import('@astrojs/starlight/expressive-code').StarlightExpressiveCodeOptions} */
 export default {
-  plugins: [pluginLineNumbers()],
-  defaultProps: {
-    overridesByLang: {
-      bash: {
-        showLineNumbers: false,
-      },
-    },
-  },
+	plugins: [pluginLineNumbers()],
+	defaultProps: {
+		overridesByLang: {
+			bash: {
+				showLineNumbers: false,
+			},
+		},
+	},
 };
