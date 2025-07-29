75eb83270c48cd4eef533a905eaaef60a9aeac3a
Oliver Speir <115520730+OliverSpeir@users.noreply.github.com>
2025-07-29 02:47:10 -0600

    format

--- playground/integration/index.ts
+++ playground/integration/index.ts
@@ -2,38 +2,38 @@ import { readFileSync } from "node:fs";
 import { z } from "astro/zod";
 
 const { addVirtualImports, createResolver, defineIntegration } = await import(
-    "astro-integration-kit"
+	"astro-integration-kit"
 );
 
 const optionsSchema = z.object({}).optional();
 
 const playgroundIntegration = defineIntegration({
-    name: "playground-integration",
-    optionsSchema,
-    setup: ({ name }) => {
-        const { resolve } = createResolver(import.meta.url);
-        return {
-            hooks: {
-                "astro:config:setup": (params) => {
-                    params.logger.info("Hello from the playground test-integration!");
+	name: "playground-integration",
+	optionsSchema,
+	setup: ({ name }) => {
+		const { resolve } = createResolver(import.meta.url);
+		return {
+			hooks: {
+				"astro:config:setup": (params) => {
+					params.logger.info("Hello from the playground test-integration!");
 
-                    addVirtualImports(params, {
-                        name,
-                        imports: {
-                            "virtual:astro-integration-kit-playground":
-                                'export const foo = "bar 1"',
-                        },
-                    });
-                },
-                "astro:config:done": ({ injectTypes }) => {
-                    injectTypes({
-                        filename: "playground-integration.d.ts",
-                        content: readFileSync(resolve("./virtual.d.ts"), "utf-8"),
-                    });
-                },
-            },
-        };
-    },
+					addVirtualImports(params, {
+						name,
+						imports: {
+							"virtual:astro-integration-kit-playground":
+								'export const foo = "bar 1"',
+						},
+					});
+				},
+				"astro:config:done": ({ injectTypes }) => {
+					injectTypes({
+						filename: "playground-integration.d.ts",
+						content: readFileSync(resolve("./virtual.d.ts"), "utf-8"),
+					});
+				},
+			},
+		};
+	},
 });
 
 export default playgroundIntegration;
